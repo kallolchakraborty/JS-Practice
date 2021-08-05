f891eed2095b69f5c6cdaad1d615c7f0a22126ea
Kallol Chakraborty <kallol.a.chakraborty@gmail.com>
2021-08-05 19:56:59 +0530

    init

---
+++ Variables.js
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------
+    author: kallol chakraborty
+    program no: 002
+    program description: variables & printing their values
+    time complexity: O(1)
+    space complexity: O(1)
+    date: 04/08/2021
+----------------------------------------------------------*/
+
+// comments
+var fullName = "Kallol Chakraborty";
+console.log(fullName);
+
+fullName = "Javascript";
+console.log(fullName);
+console.log("The full name is", fullName);
+
+// interpolation
+console.log(`
+    The full name is: ${fullName}
+`);
+
+var undefinedVariable;
+console.log(undefinedVariable);
